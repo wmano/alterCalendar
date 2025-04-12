a6a29207c5306704c33da92b2a3c53ca4276d88d
wmano <w.manolis@outlook.com>
2025-04-12 23:15:57 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import calendar from "@/components/calendar.vue";
 </script>
 
