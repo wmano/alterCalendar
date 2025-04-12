0ecd597c12014d1e42899d46558cbcd947d33801
wmano <w.manolis@outlook.com>
2025-04-12 23:07:35 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/wmano/',
+  base: '/alterCalendar/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
